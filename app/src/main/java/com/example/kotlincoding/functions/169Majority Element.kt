1012b314a08d4d5ff7fb9d679dbb05428e2c918b
Azamali010 <azamali07711@gmail.com>
2024-09-13 23:36:01 +0530

    Majirity Elements1

--- app/src/main/java/com/example/kotlincoding/functions/169Majority Element.kt
+++ app/src/main/java/com/example/kotlincoding/functions/169Majority Element.kt
@@ -1,2 +1,24 @@
 package com.example.kotlincoding.functions
 
+fun majorityElement(nums: IntArray): Int {
+    var candidate = nums[0]
+    var count = 0
+
+    for (num in nums) {
+        if (count == 0) {
+            candidate = num
+        }
+        if (num == candidate) {
+            count++
+        } else {
+            count--
+        }
+    }
+    return candidate
+}
+
+fun main() {
+    val nums = intArrayOf(2, 2, 1, 1, 1, 2, 2) // Example input array
+    val result = majorityElement(nums)
+    println("The majority element is: $result")
+}
