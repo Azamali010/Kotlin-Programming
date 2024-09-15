71a68581ff9d436d357ae22dcdaa051046a5213c
Azamali010 <azamali07711@gmail.com>
2024-09-15 22:52:44 +0530

    YES

---
+++ app/src/main/java/com/example/kotlincoding/functions/searching/BinarySearch.kt
@@ -0,0 +1,17 @@
+package com.example.kotlincoding.functions.searching
+
+fun main() {
+
+        val T = readln().toInt()
+
+        repeat(T){
+            val X = readln().toInt()
+
+            if(X>=30){
+                println("YES")
+            }else{
+                println("NO")
+            }
+
+        }
+}
